d3d9e387622a732f14a8968b03997ff06e1d7491
Ashik Rabbani <ashik.e.rabbani@gmail.com>
2019-04-15 13:58:59 +0600

    Add files via upload

---
+++ Emotion Detection.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 10 03:29:41 2019
+
+@author: Ashik
+"""
+
+import nltk.classify.util
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import movie_reviews
+
+def extract_features(word_list):
+    return dict([(word, True) for word in word_list])
+
+if __name__=='__main__':
+   positive_fileids = movie_reviews.fileids('pos')
+   negative_fileids = movie_reviews.fileids('neg')
+   
+   features_positive = [(extract_features(movie_reviews.words(fileids=[f])), 
+           'Positive') for f in positive_fileids]
+   features_negative = [(extract_features(movie_reviews.words(fileids=[f])), 
+           'Negative') for f in negative_fileids]
+                                                              
+                                                              # Split the data into train and test (80/20)
+   threshold_factor = 0.8
+   threshold_positive = int(threshold_factor * len(features_positive))
+   threshold_negative = int(threshold_factor * len(features_negative))
+   
+   features_train = features_positive[:threshold_positive] + features_negative[:threshold_negative]
+   features_test = features_positive[threshold_positive:] + features_negative[threshold_negative:]  
+   print "\nNumber of training datapoints:", len(features_train)
+   print "Number of test datapoints:", len(features_test)
+   
+   # Train a Naive Bayes classifier
+   classifier = NaiveBayesClassifier.train(features_train)
+   print "\nAccuracy of the classifier:", nltk.classify.util.accuracy(classifier, features_test)
+   
+   print "\nTop 10 most informative words:"
+   for item in classifier.most_informative_features()[:10]:
+       print item[0]
+
+# Sample input reviews
+   input_reviews = [
+       "It is an amazing movie", 
+       "This is a dull movie. I would never recommend it to anyone.",
+       "The cinematography is pretty great in this movie", 
+       "The direction was terrible and the story was all over the place" 
+   ]
